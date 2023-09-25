2a9193c93f8964eed80f829479e569292d2b2559
M. Eren Akbiyik <erenakbiyik@gmail.com>
2023-09-25 13:24:31 +0300

    ci: bump to v0.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath(".." + os.sep + ".."))
 project = "torchcache"
 copyright = "2023, Eren Akbiyik"
 author = "Eren Akbiyik"
-release = "v0.3.2"
+release = "v0.4.0"
 
 html_title = "torchcache"
 
